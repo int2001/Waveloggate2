2da2b0a3082b4e17bba24f6790080bd0c218be10
Joerg <joerg@dj7nt.de>
2026-03-04 07:39:41 +0100

    Truncate QRG to Hz (no need for millihertz) when shown

--- frontend/src/components/StatusTab.svelte
+++ frontend/src/components/StatusTab.svelte
@@ -31,7 +31,7 @@
   onMount(async () => {
     offRadio = EventsOn("radio:status", (data) => {
       if (data && data.freqMHz !== undefined) {
-        freqMHz = Number(data.freqMHz).toFixed(6);
+        freqMHz = Number(data.freqMHz).toFixed(5);
         mode = data.mode || "";
       }
     });
